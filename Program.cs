fc596d66f6b775f912c0be296702170de450c047
vetemens <dan13fifa3@gmail.com>
2022-02-13 15:59:33 +0200

    Fourth commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace user
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User SomeUser = new User();
+            SomeUser.Login = "format1cle";
+            SomeUser.Name = "Illya";
+            SomeUser.Surname = "Korobkin";
+            SomeUser.Age = 21;
+
+            Console.WriteLine("Информация про користувача:\nЛогин: {0}\nИмя: {1}\nФамилия: {2}\nВозраст: {3}\nДата створення: {4}", SomeUser.Login, SomeUser.Name, SomeUser.Surname, SomeUser.Age, DateTime.Now);
+
+
+
+
+        }
+
+
+
+
+    }
+
+
+
+
+
+    
+}
---
+++ User.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace user
+{
+    class User
+    {
+        string login;
+        string name;
+        string surname;
+        int age;
+        public DateTime date;
+
+        public string Login
+        {
+            set { login = value; }
+            get
+            {
+                if (login == null)
+                    return "Не заповнено!";
+                else
+                    return login;
+            }
+        
+        }
+        public string Name
+        {
+            set { name = value; }
+            get
+            {
+                if (name == null)
+                    return "Не заповнено!";
+                else
+                    return name;
+
+            }
+        }
+        public string Surname
+        {
+            set { surname = value; }
+            get
+            {
+                if (surname == null)
+                    return "Не заповнено!";
+                else 
+                    return surname;
+
+
+            }
+        }
+        public int Age
+        {
+            set { age = value; }
+            get
+            {
+                if (age <= 0)
+                    return 18;
+                else
+                    return age;
+            }
+        }
+        public User()
+        {
+            date = DateTime.Now;    
+        }
+        public User(string login,string name, string surname, int age)
+        {
+            this.Login = login;
+            this.Name = name;
+            this.Surname = surname;
+            this.Age = age; 
+            date = DateTime.Now;
+        }
+
+    }
+
+}
