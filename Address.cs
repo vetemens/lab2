930f5f33d8989197671eb669b8ab4897f2c95489
vetemens <dan13fifa3@gmail.com>
2022-02-13 11:21:20 +0200

    first commit

---
+++ Address.cs
@@ -0,0 +1,84 @@
+﻿
+namespace lab2
+{
+    class Address
+    {
+        private int index;
+        private string country;
+        private string city;
+        private string street;
+        private int house;
+        private int apartment;
+
+        public int Index
+        {
+            get
+            {
+                return index;
+            }
+            set
+            {
+                index = value;
+            }
+
+        }
+        public string Name
+        {
+            get { return country; }
+            set { country = value; }
+        }
+        public string Name1
+        {
+            get { return city; }
+            set { city = value; }
+        }
+        public string Name2
+        {
+            get { return street; }
+            set { street = value; }
+        }
+        public int House
+        {
+            get { return house; }
+            set { house = value; }
+        }
+        public int Apartment
+        {
+            get { return apartment; }
+            set { apartment = value; }
+        }
+        
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
--- Program.cs
+++ Program.cs
@@ -1,84 +1,29 @@
-﻿class Address
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
 {
-    public int index;
-    public string country;
-    public string city;
-    public string street;
-    public int house;
-    public int apartment;
-
-    public int Index
-    {
-        get
-        {
-            return index;
-        }
-        set
-        {
-            index = value;
-        }
-
-    }
-    public string Name
-    {
-        get { return country; }
-        set { country = value; }
-    }
-    public string Name1
-    {
-        get { return city; }
-        set { city = value; }
-    }
-    public string Name2
-    {
-        get { return street; }
-        set { street = value; }
-    }
-    public int House
-    {
-        get { return house; }
-        set { house = value; }
-    }
-    public int Apartment
-    {
-        get { return apartment; }
-        set { apartment = value; }
-    }
-class Myaddress
+    class Program
     {
         static void Main(string[] args)
         {
             Address a = new Address();
-            a.index = 08300;
-            a.country = "Ukraine";
-            a.city = "Kiev";
-            a.street = "Shevchenka";
-            a.house = 228;
-            a.apartment = 17;
-            Console.WriteLine($"index:{a.index}, country:{a.country}, city:{a.city}, street:{a.street}, house:{a.house}, apartment:{a.apartment},");
+            a.Index = 08300;
+            a.Name = "Ukraine";
+            a.Name1 = "Kiev";
+            a.Name2 = "Shevchenka";
+            a.House = 228;
+            a.Apartment = 17;
+            Console.WriteLine($"index:{a.Index}, country:{a.Name}, city:{a.Name1}, street:{a.Name2}, house:{a.House}, apartment:{a.Apartment},");
 
 
         }
-            
+
 
 
 
     }
-
-
-
-
-
-
-
-
-
 }
-
-
-
-
-
-
-
-
